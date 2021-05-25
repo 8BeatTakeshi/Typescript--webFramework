868f89c55f8edb7978a0ef888d357f262626232d
8BeatTakeshi <d.nakamura75@gmail.com>
2021-05-25 15:21:23 +0200

    create a User Class and add get method to retrieve properties

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,6 @@
-console.log('App is running');
+import { User } from './models/User';
+
+const newUser = new User({ name: 'John', age: 48 });
+
+console.log(newUser.get('name'));
+console.log(newUser.get('age'));
