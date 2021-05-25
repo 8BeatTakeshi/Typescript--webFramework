d8dec1a08a0727111a2f510ffb5b496e3324467a
8BeatTakeshi <d.nakamura75@gmail.com>
2021-05-25 16:15:07 +0200

    add "trigger" method in User Class to trigger event callbacks

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,16 @@ import { User } from './models/User';
 
 const user = new User({ name: 'John', age: 48 });
 
-user.on('change', () => {});
-user.on('change', () => {});
-user.on('onclick', () => {});
+user.on('change', () => {
+  console.log('Change #1');
+});
+user.on('change', () => {
+  console.log('Change #2');
+});
+user.on('onclick', () => {
+  console.log('Onclick #1');
+});
+
+user.trigger('jkljlkjlklklkjl');
 
 console.log(user);
