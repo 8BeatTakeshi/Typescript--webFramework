ab8b671d2fb0a9f75dea5babcc6a39799633e8df
8BeatTakeshi <d.nakamura75@gmail.com>
2021-05-25 16:54:41 +0200

    install axios

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,8 @@
-import { User } from './models/User';
+import axios from 'axios';
 
-const user = new User({ name: 'John', age: 48 });
+// axios.post('http://localhost:3000/users', {
+//   name: 'Sonic',
+//   age: 20,
+// });
 
-user.on('change', () => {
-  console.log('Change #1');
-});
-user.on('change', () => {
-  console.log('Change #2');
-});
-user.on('onclick', () => {
-  console.log('Onclick #1');
-});
-
-user.trigger('jkljlkjlklklkjl');
-
-console.log(user);
+axios.get('http://localhost:3000/users/1');
