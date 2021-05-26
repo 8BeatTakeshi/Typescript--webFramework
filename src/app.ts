0d3e525f80df93f758d781bf4636de79a2681e6b
8BeatTakeshi <d.nakamura75@gmail.com>
2021-05-26 16:30:36 +0200

    refactor get and set  method in User Class with Composition

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,3 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'Mario', age: 40 });
-
-console.log(user);
-
-user.sync.save({ name: 'Mario', age: 40 });
+// user.sync.save({ name: 'Mario', age: 40 });
