aa70e553087ddbc4274b03a0598f39bfc33d8262
8BeatTakeshi <d.nakamura75@gmail.com>
2021-05-28 17:09:16 +0200

    implement set method in User Class

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,13 @@ import { User } from './models/User';
 const user = new User({ name: 'Jinx', age: 19 });
 
 console.log(user.get('name'));
+console.log(user.get('age'));
 
 user.on('change', () => {
-  console.log('User was changed.');
+  console.log('User was changed, we propably need to update some Html');
 });
 
-user.trigger('change');
+user.set({ name: 'Sonic' });
+
+console.log(user.get('name'));
+console.log(user.get('age'));
--- src/models/User.ts
+++ src/models/User.ts
@@ -19,15 +19,21 @@ export class User {
     this.attributes = new Attributes<UserProps>(attrs);
   }
 
-  get get() {
-    return this.attributes.get;
-  }
-
   get on() {
     return this.events.on;
   }
 
   get trigger() {
     return this.events.trigger;
+  }
+
+  get get() {
+    return this.attributes.get;
+  }
+
+  set(update: Partial<UserProps>): void {
+    this.attributes.set(update);
+
+    this.events.trigger('change');
   }
 }
